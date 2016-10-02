94c4cc09ef54fa0ab87fcccfc11e0613276d781e
vasubabu <jinagamvasubabu@gmail.com>
2016-10-02 19:09:04 +0530

    react boilerplate code

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './app/index.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx/,
+      loaders: ["babel"],
+      include: path.join(__dirname, 'app')
+    }]
+  }
+};
